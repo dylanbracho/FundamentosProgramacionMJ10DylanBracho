4c154d2650a4cc50500725ac0f353b1fa9291cae
dylanbracho <dylan.bracho@upb.edu.co>
2026-07-28 11:31:24 -0500

    Mi primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aqui escribo codigo fuente
+            string nombre = "Dylan";
+            Console.WriteLine("Bienvenido " + nombre + " al curso de fundamentos de programación");
+        }
+    }
+}
