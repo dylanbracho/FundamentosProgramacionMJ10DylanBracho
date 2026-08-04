a76f347458768c947f142f8b6a0c8e5294799f02
dylanbracho <dylan.bracho@upb.edu.co>
2026-08-04 11:13:25 -0500

    Operadores lógicos comparación

---
+++ 3.Operadores/3.Operadores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _3.Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Orden de evaluacion operadores numericos
+            float dato1 = 4 * 3 / 2;
+            float dato2 = 4f / 3 * 2;
+            float dato3 = 4f * (2f / 3);
+            float dato4 = 4 + 6 * 3;
+            float dato5 = 4 + 6 * (2 - 1);
+            Console.WriteLine(dato4);
+
+            //Operadores logicos
+            //Conjuncion - AND - Y - &&
+            Console.WriteLine("Tabla Conjuncion");
+            Console.WriteLine("V && V = " + (true && true));
+            Console.WriteLine("V && F = " + (true && true));
+            Console.WriteLine("F && V = " + (  false && true));
+            Console.WriteLine("F && F = " + (false && false));
+            Console.WriteLine("---------------------------");
+
+            //Disyuncion - OR - O - ||
+            Console.WriteLine("Tabla Disyuncion");
+            Console.WriteLine("V || V = " + (true || true));
+            Console.WriteLine("V || F = " + (true || true));
+            Console.WriteLine("F || V = " + (false || true));
+            Console.WriteLine("F || F = " + (false || false));
+            Console.WriteLine("---------------------------");
+            //Operador logico de negacion !
+            bool dato8 = true;
+            bool dato9 = !dato8;
+
+            //Operadores de comparacion
+            bool dato6 = 5 > 4;
+            bool dato7 = 100 == 99;
+            bool dato10 = 1002 != 102;
+            bool dato11 = 10 >= 10;
+            bool dato12 = 12 < 11 && 0 == 1;
+            bool dato13 = dato8 && 0 != 10 || !dato10;
+        }
+    }
+}
