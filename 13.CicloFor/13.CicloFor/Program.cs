8202130c72a3f2fb06d97ccc5e26ed6512ab8ed8
dylanbracho <dylan.bracho@upb.edu.co>
2026-09-03 11:26:29 -0500

    Ciclo For

---
+++ 13.CicloFor/13.CicloFor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ciclo For
+            int acumulador = 0;
+
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros numeros enteros es: {acumulador}");
+        }
+    }
+}
